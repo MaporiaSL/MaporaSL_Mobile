bbdcf35300ddb77834ddec41ce5a72e1fc8d2213
hitheshi <kariyawasamhitheshi16@gmail.com>
2026-02-09 19:54:38 +0530

    feat: add initial implementation of SplashScreen widget

---
+++ mobile/lib/splash/presentation/splash_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFF1E7BF6),
+      body: Text("Splash Screen")
+    );
+  }
+}
