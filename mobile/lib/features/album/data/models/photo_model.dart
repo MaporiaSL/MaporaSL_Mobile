74877852fb619c4f47581afcfac46bb8b2b6891f
hitheshi <kariyawasamhitheshi16@gmail.com>
2026-02-13 12:45:44 +0530

    feat: add PhotoLocation model for album feature

---
+++ mobile/lib/features/album/data/models/photo_model.dart
@@ -0,0 +1,7 @@
+class PhotoLocation {
+  final double? latitude;
+  final double? longitude;
+  final String? placeName;
+
+  const PhotoLocation({this.latitude, this.longitude, this.placeName});
+}
