662f90b0928dd0fa585830464d8934b8ead51861
hitheshi <kariyawasamhitheshi16@gmail.com>
2026-02-13 15:31:39 +0530

    feat: add factory method for PhotoLocation model to create instances from JSON

--- mobile/lib/features/album/data/models/photo_model.dart
+++ mobile/lib/features/album/data/models/photo_model.dart
@@ -4,4 +4,12 @@ class PhotoLocation {
   final String? placeName;
 
   const PhotoLocation({this.latitude, this.longitude, this.placeName});
+
+  factory PhotoLocation.fromJson(Map<String, dynamic> json) {
+    return PhotoLocation(
+      latitude: json['latitude']?.toDouble(),
+      longitude: json['longitude']?.toDouble(),
+      placeName: json['placeName'] as String?,
+    );
+  }
 }
