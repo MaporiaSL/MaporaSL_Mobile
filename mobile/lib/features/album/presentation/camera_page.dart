f4ac7f6ae193bfbf984be140f88ea6a6515700ba
hitheshi <kariyawasamhitheshi16@gmail.com>
2026-02-20 14:15:17 +0530

    feat: add CameraPage widget for capturing photos

---
+++ mobile/lib/features/album/presentation/camera_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class CameraPage extends StatefulWidget {
+  const CameraPage({super.key});
+
+  @override
+  State<CameraPage> createState() => _CameraPageState();
+}
+
+class _CameraPageState extends State<CameraPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text(
+          "Camera Page",
+          style: TextStyle(fontSize: 20),
+        ),
+      ),
+    );
+  }
+}
