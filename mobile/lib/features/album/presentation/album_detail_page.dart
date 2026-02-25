8db34abf94bdc3a14aa837e16dc1568489856b2f
Hitheshi Kariyawasam <kariyawasamhitheshi16@gmail.com>
2026-02-25 14:29:18 +0530

    feat: initialize AlbumDetailPage with album service and photo list

--- mobile/lib/features/album/presentation/album_detail_page.dart
+++ mobile/lib/features/album/presentation/album_detail_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import '../data/services/album_service.dart';
 import '../data/models/album_model.dart';
+import '../data/models/photo_model.dart';
 
 class AlbumDetailPage extends StatefulWidget {
   final AlbumModel album;
@@ -11,6 +13,11 @@ class AlbumDetailPage extends StatefulWidget {
 }
 
 class _AlbumDetailPageState extends State<AlbumDetailPage> {
+  final AlbumService _service = AlbumService();
+
+  List<PhotoModel> _photos = [];
+  bool _isLoading = true;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
