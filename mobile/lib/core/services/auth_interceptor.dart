6b54ee9b739b823ff02a69dfddee37fe729e5eba
Pudamya Yamini De Silva <desilvapudamya@gmail.com>
2026-03-19 12:50:30 +0530

    feat: implement error handling and token refresh in AuthInterceptor

--- mobile/lib/core/services/auth_interceptor.dart
+++ mobile/lib/core/services/auth_interceptor.dart
@@ -5,6 +5,9 @@ class AuthInterceptor extends Interceptor {
   AuthInterceptor({FirebaseAuth? auth}) : _auth = auth ?? FirebaseAuth.instance;
 
   final FirebaseAuth _auth;
+  final Dio _retryDio = Dio();
+
+  static const String _retryFlag = 'authRetryAttempted';
 
   @override
   void onRequest(
@@ -17,5 +20,29 @@ class AuthInterceptor extends Interceptor {
       options.headers['Authorization'] = 'Bearer $token';
     }
     handler.next(options);
+  }
+
+  @override
+  void onError(DioException err, ErrorInterceptorHandler handler) async {
+    final requestOptions = err.requestOptions;
+    final isUnauthorized = err.response?.statusCode == 401;
+    final alreadyRetried = requestOptions.extra[_retryFlag] == true;
+    final user = _auth.currentUser;
+
+    if (!isUnauthorized || alreadyRetried || user == null) {
+      handler.next(err);
+      return;
+    }
+
+    try {
+      final freshToken = await user.getIdToken(true);
+      requestOptions.headers['Authorization'] = 'Bearer $freshToken';
+      requestOptions.extra[_retryFlag] = true;
+
+      final retryResponse = await _retryDio.fetch<dynamic>(requestOptions);
+      handler.resolve(retryResponse);
+    } catch (_) {
+      handler.next(err);
+    }
   }
 }
